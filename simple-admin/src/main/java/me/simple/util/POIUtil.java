b643b8fd4d9ff5a0bc42a84017e1f1904234b54e
chensheng <jrchens@126.com>
2017-03-20 23:25:30 +0800

    poi date cell

--- simple-admin/src/main/java/me/simple/util/POIUtil.java
+++ simple-admin/src/main/java/me/simple/util/POIUtil.java
@@ -20,15 +20,15 @@ public class POIUtil {
     private static final String dateFormatPattern = "yyyy-MM-dd";
     private static final DecimalFormat numberFormat = new DecimalFormat();
     private static final String numberFormatPattern = "#0.##";
-    
+
     /**
-     * The following patterns are used in {@link #isADateFormat(int, String)}
+     * The following patterns are used in {@link #isADateFormat(String)}
      */
     private static final Pattern date_ptrn1 = Pattern.compile("^\\[\\$\\-.*?\\]");
     private static final Pattern date_ptrn2 = Pattern.compile("^\\[[a-zA-Z]+\\]");
-    private static final Pattern date_ptrn3a = Pattern.compile("[yYmMdDhHsS\u5e74\u6708\u65e5\u65f6\u5206\u79d2]"); // 年月日时分秒
-    private static final Pattern date_ptrn3b = Pattern.compile("^[\\[\\]yYmMdDhHsS\u5e74\u6708\u65e5\u65f6\u5206\u79d2\\-T/,. :\"\\\\]+0*[ampAMP/]*$");
-    //  elapsed time patterns: [h],[m] and [s]
+    private static final Pattern date_ptrn3a = Pattern.compile("[yYmMdDhHsS]");
+    private static final Pattern date_ptrn3b = Pattern.compile("^[\\[\\]yYmMdDhHsS\\-T/,. :\"\\\\]+0*[ampAMP/]*$");
+    // elapsed time patterns: [h],[m] and [s]
     private static final Pattern date_ptrn4 = Pattern.compile("^\\[([hH]+|[mM]+|[sS]+)\\]");
 
     public static boolean isValidRow(Row row) {
@@ -36,133 +36,134 @@ public class POIUtil {
 	    return false;
 	}
 	for (Cell cell : row) {
-	    if (cell != null && StringUtils.hasText(getCellStringValue(cell, null))) {
+	    if (cell != null && StringUtils.hasText(getCellStringValue(cell))) {
 		return true;
 	    }
 	}
 	return false;
     }
-    
+
     /**
-     * copy form org.apache.poi.ss.usermodel.DateUtil.isADateFormat(int formatIndex, String formatString)
+     * copy form org.apache.poi.ss.usermodel.DateUtil.isADateFormat(int
+     * formatIndex, String formatString)
+     * 
      * @param formatIndex
      * @param formatString
      * @return
      */
-    private static boolean isADateFormat(int formatIndex, String formatString) {
-        // First up, is this an internal date format?
-//        if(isInternalDateFormat(formatIndex)) {
-//            cache(formatString, formatIndex, true);
-//            return true;
-//        }
-	
-        // If we didn't get a real string, don't even cache it as we can always find this out quickly
-//        if(formatString == null || formatString.length() == 0) {
-//            return false;
-//        }
+    public static boolean isADateFormat(/* int formatIndex, */ String formatString) {
+	// First up, is this an internal date format?
+	// if(isInternalDateFormat(formatIndex)) {
+	// cache(formatString, formatIndex, true);
+	// return true;
+	// }
 
-        // check the cache first
-//        if (isCached(formatString, formatIndex)) {
-//            return lastCachedResult.get();
-//        }
+	// If we didn't get a real string, don't even cache it as we can always
+	// find this out quickly
+	// if(formatString == null || formatString.length() == 0) {
+	// return false;
+	// }
 
-        String fs = formatString;
-        /*if (false) {
-            // Normalize the format string. The code below is equivalent
-            // to the following consecutive regexp replacements:
+	// check the cache first
+	// if (isCached(formatString, formatIndex)) {
+	// return lastCachedResult.get();
+	// }
 
-             // Translate \- into just -, before matching
-             fs = fs.replaceAll("\\\\-","-");
-             // And \, into ,
-             fs = fs.replaceAll("\\\\,",",");
-             // And \. into .
-             fs = fs.replaceAll("\\\\\\.",".");
-             // And '\ ' into ' '
-             fs = fs.replaceAll("\\\\ "," ");
+	String fs = formatString;
+	/*
+	 * if (false) { // Normalize the format string. The code below is
+	 * equivalent // to the following consecutive regexp replacements:
+	 * 
+	 * // Translate \- into just -, before matching fs =
+	 * fs.replaceAll("\\\\-","-"); // And \, into , fs =
+	 * fs.replaceAll("\\\\,",","); // And \. into . fs =
+	 * fs.replaceAll("\\\\\\.","."); // And '\ ' into ' ' fs =
+	 * fs.replaceAll("\\\\ "," ");
+	 * 
+	 * // If it end in ;@, that's some crazy dd/mm vs mm/dd // switching
+	 * stuff, which we can ignore fs = fs.replaceAll(";@", "");
+	 * 
+	 * // The code above was reworked as suggested in bug 48425: // simple
+	 * loop is more efficient than consecutive regexp replacements. }
+	 */
+	StringBuilder sb = new StringBuilder(fs.length());
+	for (int i = 0; i < fs.length(); i++) {
+	    char c = fs.charAt(i);
 
-             // If it end in ;@, that's some crazy dd/mm vs mm/dd
-             //  switching stuff, which we can ignore
-             fs = fs.replaceAll(";@", "");
+	    // 年月日时分秒 " '
+	    switch (c) {
+	    case '\u5e74':
+	    case '\u6708':
+	    case '\u65e5':
+	    case '\u65f6':
+	    case '\u5206':
+	    case '\u79d2':
+		continue;
+	    }
 
-             // The code above was reworked as suggested in bug 48425:
-             // simple loop is more efficient than consecutive regexp replacements.
-        }*/
-        StringBuilder sb = new StringBuilder(fs.length());
-        for (int i = 0; i < fs.length(); i++) {
-            char c = fs.charAt(i);
-            if (i < fs.length() - 1) {
-                char nc = fs.charAt(i + 1);
-                if (c == '\\') {
-                    switch (nc) {
-                        case '-':
-                        case ',':
-                        case '.':
-                        case ' ':
-                        case '\\':
-                            // skip current '\' and continue to the next char
-                            continue;
-                    }
-                } else if (c == ';' && nc == '@') {
-                    i++;
-                    // skip ";@" duplets
-                    continue;
-                }
-            }
-            sb.append(c);
-        }
-        fs = sb.toString();
+	    if (i < fs.length() - 1) {
 
-        // short-circuit if it indicates elapsed time: [h], [m] or [s]
-        if(date_ptrn4.matcher(fs).matches()){
-//            cache(formatString, formatIndex, true);
-            return true;
-        }
+		char nc = fs.charAt(i + 1);
+		if (c == '\\') {
+		    switch (nc) {
+		    case '-':
+		    case ',':
+		    case '.':
+		    case ' ':
+		    case '\\':
+			// skip current '\' and continue to the next char
+			continue;
+		    }
+		} else if (c == ';' && nc == '@') {
+		    i++;
+		    // skip ";@" duplets
+		    continue;
+		}
+	    }
+	    sb.append(c);
+	}
+	fs = sb.toString();
 
-        // If it starts with [$-...], then could be a date, but
-        //  who knows what that starting bit is all about
-        fs = date_ptrn1.matcher(fs).replaceAll("");
-        // If it starts with something like [Black] or [Yellow],
-        //  then it could be a date
-        fs = date_ptrn2.matcher(fs).replaceAll("");
-        // You're allowed something like dd/mm/yy;[red]dd/mm/yy
-        //  which would place dates before 1900/1904 in red
-        // For now, only consider the first one
-        if(fs.indexOf(';') > 0 && fs.indexOf(';') < fs.length()-1) {
-           fs = fs.substring(0, fs.indexOf(';'));
-        }
+	// short-circuit if it indicates elapsed time: [h], [m] or [s]
+	if (date_ptrn4.matcher(fs).matches()) {
+	    // cache(formatString, formatIndex, true);
+	    return true;
+	}
 
-        // Ensure it has some date letters in it
-        // (Avoids false positives on the rest of pattern 3)
-        if (! date_ptrn3a.matcher(fs).find()) {
-           return false;
-        }
-        
-        // If we get here, check it's only made up, in any case, of:
-        //  y m d h s - \ / , . : [ ] T
-        // optionally followed by AM/PM
+	// If it starts with [$-...], then could be a date, but
+	// who knows what that starting bit is all about
+	fs = date_ptrn1.matcher(fs).replaceAll("");
+	// If it starts with something like [Black] or [Yellow],
+	// then it could be a date
+	fs = date_ptrn2.matcher(fs).replaceAll("");
+	// You're allowed something like dd/mm/yy;[red]dd/mm/yy
+	// which would place dates before 1900/1904 in red
+	// For now, only consider the first one
+	if (fs.indexOf(';') > 0 && fs.indexOf(';') < fs.length() - 1) {
+	    fs = fs.substring(0, fs.indexOf(';'));
+	}
 
-        boolean result = date_ptrn3b.matcher(fs).matches();
-        // cache(formatString, formatIndex, result);
-        return result;
-    }
-    
-    public static boolean isDateCell(Cell cell) {
-	if (cell == null) {
+	// Ensure it has some date letters in it
+	// (Avoids false positives on the rest of pattern 3)
+	if (!date_ptrn3a.matcher(fs).find()) {
 	    return false;
 	}
-	int type = cell.getCellType();
-	if (type == Cell.CELL_TYPE_NUMERIC) {
-	    CellStyle cellStyle = cell.getCellStyle();
-	    short format = cellStyle.getDataFormat();
-	    String formatString = cellStyle.getDataFormatString();
-	    if (DateUtil.isCellDateFormatted(cell) || isADateFormat(format, formatString)) {
-		return true;
-	    }
-	}
-	return false;
+
+	// If we get here, check it's only made up, in any case, of:
+	// y m d h s - \ / , . : [ ] T
+	// optionally followed by AM/PM
+
+	boolean result = date_ptrn3b.matcher(fs).matches();
+	// cache(formatString, formatIndex, result);
+	return result;
     }
 
-    public static String getCellStringValue(Cell cell, String pattern) {
+    /**
+     * 
+     * @param cell
+     * @return #0.## | yyyy-MM-dd | cell.getStringCellValue()
+     */
+    public static String getCellStringValue(Cell cell) {
 	if (cell == null) {
 	    return "";
 	}
@@ -172,39 +173,16 @@ public class POIUtil {
 	    double _value = cell.getNumericCellValue();
 	    CellStyle cellStyle = cell.getCellStyle();
 	    String formatString = cellStyle.getDataFormatString();
-	    short format = cellStyle.getDataFormat();
-	    /*
-	     * boolean isDate = false; if("General".equals(formatString) ||
-	     * "@".equals(formatString)){ formatString = "#0.##"; }else
-	     * if(formatString.indexOf(";@") != -1 &&
-	     * (formatString.indexOf("yy") != -1 || formatString.indexOf("m") !=
-	     * -1 || formatString.indexOf("d") != -1 ||
-	     * formatString.indexOf("h") != -1 || formatString.indexOf("m") !=
-	     * -1 || formatString.indexOf("s") != -1)){ isDate = true; }
-	     */
-	    if (DateUtil.isCellDateFormatted(cell) || DateUtil.isCellInternalDateFormatted(cell)
-		    || DateUtil.isADateFormat(format, formatString) || DateUtil.isInternalDateFormat(format)
-	    /*
-	     * || isDate || DateUtil.isValidExcelDate(_value)
-	     */) {
-		if (StringUtils.hasText(pattern)) {
-		    dateFormat.applyPattern(pattern);
-		} else {
-		    dateFormat.applyPattern(dateFormatPattern);
-		}
+	    if (DateUtil.isCellDateFormatted(cell) || isADateFormat(formatString)) {
+		dateFormat.applyPattern(dateFormatPattern);
 		value = dateFormat.format(DateUtil.getJavaDate(_value));
 	    } else {
-		if (StringUtils.hasText(pattern)) {
-		    numberFormat.applyPattern(pattern);
-		} else {
-		    numberFormat.applyPattern(numberFormatPattern);
-		}
+		numberFormat.applyPattern(numberFormatPattern);
 		value = numberFormat.format(cell.getNumericCellValue());
 	    }
 	} else if (type == Cell.CELL_TYPE_STRING) {
 	    value = cell.getStringCellValue();
 	}
-	// cell.setCellType(Cell.CELL_TYPE_STRING);
 	return StringUtils.trimWhitespace(ObjectUtils.getDisplayString(value));
     }
 
@@ -213,32 +191,32 @@ public class POIUtil {
 	if (sheet == null) {
 	    return instants;
 	}
-	
+
 	int rowNum = sheet.getLastRowNum();
-	int[] cellNums = {0,0};
+	int[] cellNums = { 0, 0 };
 	if (rowNum > 1) {
 	    for (int i = 0; i < 2; i++) {
 		Row row = sheet.getRow(i);
 		// int cellNum = row.getLastCellNum();
 		List<String> rowData = Lists.newArrayList();
-		 int index = 0;
+		int index = 0;
 		for (Cell cell : row) {
-		    String value = getCellStringValue(cell, null);
+		    String value = getCellStringValue(cell);
 		    if (!StringUtils.hasText(value)) {
 			throw new RuntimeException("row format error , cell value is empty");
 		    } else {
 			rowData.add(value);
 		    }
-		     index++;
+		    index++;
 		}
 		cellNums[i] = index;
 		instants.add(rowData.toArray(new String[] {}));
 	    }
-	    if(cellNums[0] != cellNums[1]){
+	    if (cellNums[0] != cellNums[1]) {
 		throw new RuntimeException("row format error , different columns of rows");
 	    }
 	}
-	
+
 	return instants;
     }
 }
--- simple-test/src/test/java/org/apache/poi/POIUtilTest.java
+++ simple-test/src/test/java/org/apache/poi/POIUtilTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DateUtil;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -17,7 +18,7 @@ import org.springframework.util.ResourceUtils;
 import me.simple.util.POIUtil;
 
 public class POIUtilTest {
-    
+
     private final static Logger logger = LoggerFactory.getLogger(POIUtilTest.class);
 
     @Test
@@ -27,12 +28,13 @@ public class POIUtilTest {
 	try {
 	    is = new FileInputStream(ResourceUtils.getFile("classpath:read.xls"));
 	    wb = new HSSFWorkbook(is);
-	    
+
 	    Sheet sheet = wb.getSheetAt(0);
 	    for (Row row : sheet) {
-		if(POIUtil.isValidRow(row)){
+		if (POIUtil.isValidRow(row)) {
 		    for (Cell cell : row) {
-			logger.info("is date cell: {}, cell value: {}",POIUtil.isDateCell(cell), POIUtil.getCellStringValue(cell, null));
+			logger.info("is date cell: {}, cell value: {}", DateUtil.isCellDateFormatted(cell),
+				POIUtil.getCellStringValue(cell));
 		    }
 		}
 	    }
@@ -41,8 +43,7 @@ public class POIUtilTest {
 	    wb.close();
 	}
     }
-    
-    
+
     @Test
     public void testGetSheetRow2() throws Exception {
 	Workbook wb = null;
@@ -50,21 +51,25 @@ public class POIUtilTest {
 	try {
 	    is = new FileInputStream(ResourceUtils.getFile("classpath:data.xls"));
 	    wb = new HSSFWorkbook(is);
-	    
+
 	    Sheet sheet = wb.getSheetAt(0);
-	    
+
 	    List<String[]> row2 = POIUtil.getSheetRow2(sheet);
 	    for (String[] row : row2) {
 		for (String cell : row) {
-		    logger.info("cell value: {}",cell);
+		    logger.info("cell value: {}", cell);
 		}
 	    }
-	    
+
 	} finally {
 	    is.close();
 	    wb.close();
 	}
     }
-    
-    
+
+    @Test
+    public void testIsADateFormat() throws Exception {
+	String formatString = "yyyy年MM月dd日";
+	logger.info("{} isADateFormat: {}", formatString, POIUtil.isADateFormat(formatString));
+    }
 }
