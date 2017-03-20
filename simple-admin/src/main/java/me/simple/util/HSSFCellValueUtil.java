9e0caba1828f6944056251e7c38be13c5b131eec
jrchens <jrchens@126.com>
2017-03-20 12:30:02 +0800

    poi date cell process

--- simple-admin/src/main/java/me/simple/util/HSSFCellValueUtil.java
+++ simple-admin/src/main/java/me/simple/util/HSSFCellValueUtil.java
@@ -1,9 +1,13 @@
 package me.simple.util;
 
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.DateUtil;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
 
 public class HSSFCellValueUtil {
     
@@ -14,7 +18,7 @@ public class HSSFCellValueUtil {
      * @param cell
      * @return
      */
-    public String getCellStringValue(Cell cell) {
+    public static String getCellStringValue(Cell cell) {
 	if (cell == null) {
 	    return "";
 	}
@@ -22,14 +26,35 @@ public class HSSFCellValueUtil {
 	int type = cell.getCellType();
 	if (type == Cell.CELL_TYPE_NUMERIC) {
 	    double _value = cell.getNumericCellValue();
-	    if (DateUtil.isCellDateFormatted(cell)) {
+	    CellStyle cellStyle = cell.getCellStyle();
+	    String formatString = cellStyle.getDataFormatString();
+	    short format = cellStyle.getDataFormat();
+	    boolean isDate = false;
+	    if("General".equals(formatString) || "@".equals(formatString)){
+		formatString = "#0.##";
+	    }else if(formatString.indexOf(";@") != -1 && 
+		    (formatString.indexOf("yy") != -1 
+		    || formatString.indexOf("m") != -1 
+		    || formatString.indexOf("d") != -1
+		    || formatString.indexOf("h") != -1
+		    || formatString.indexOf("m") != -1
+		    || formatString.indexOf("s") != -1)){
+		isDate = true;
+	    }
+	    if (DateUtil.isCellDateFormatted(cell) || DateUtil.isCellInternalDateFormatted(cell)
+		    || DateUtil.isADateFormat(format, formatString)
+		    || DateUtil.isInternalDateFormat(format)
+		    || isDate 
+		    /*|| DateUtil.isValidExcelDate(_value)*/) {
 		value = dateFormat.format(DateUtil.getJavaDate(_value));
 	    } else {
-		value = String.valueOf(cell.getNumericCellValue());
+		DecimalFormat numberFormat = new DecimalFormat(formatString);
+		value = numberFormat.format(cell.getNumericCellValue());
 	    }
 	} else if (type == Cell.CELL_TYPE_STRING) {
 	    value = cell.getStringCellValue();
 	}
-	return value;
+	// cell.setCellType(Cell.CELL_TYPE_STRING);
+	return StringUtils.trimWhitespace(ObjectUtils.getDisplayString(value));
     }
 }
