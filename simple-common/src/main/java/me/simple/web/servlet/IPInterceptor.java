39214efc4ec12e297a72180dae67992d30154ef4
chensheng <jrchens@126.com>
2017-03-25 22:49:19 +0800

    ip intercepter

--- simple-common/src/main/java/me/simple/web/servlet/IPInterceptor.java
+++ simple-common/src/main/java/me/simple/web/servlet/IPInterceptor.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -17,36 +18,27 @@ import com.google.common.collect.Lists;
 // HandlerInterceptorAdapter
 public class IPInterceptor implements HandlerInterceptor {
     private static final Logger logger = LoggerFactory.getLogger(IPInterceptor.class);
-    private static final List<String> allowedIPList = Lists.newArrayList();
+    private static final List<String> authorizedIPList = Lists.newArrayList();
     private static final List<String> deniedIPList = Lists.newArrayList();
-    
-//    static {
-//	try {
-//	    File file = ResourceUtils.getFile("classpath:config.properties");
-//	    Reader reader = new FileReader(file);
-//	} catch (Exception e) {
-//	}
-//    }
-    
+    private boolean enable = false;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 	    throws Exception {
 	String ip = request.getRemoteAddr();
 	String host = request.getRemoteHost();
-	String disabled = "1";
-	if("1".equals(disabled) || "true".equalsIgnoreCase(disabled)){
-	    return true;
-	}
-	logger.info("IP Interceptor ,  ip: {}, host: {}", ip,host);
-	if(deniedIPList.contains(ip)){
+	if (enable) {
+	    logger.info("IP Interceptor ,  ip: {}, host: {}", ip, host);
+	    if (deniedIPList.contains(ip)) {
+		// response.sendError(HttpStatus.FORBIDDEN.value());
+		return false;
+	    }
+	    if (authorizedIPList.contains(ip)) {
+		return true;
+	    }
 	    // response.sendError(HttpStatus.FORBIDDEN.value());
-	    return false;
 	}
-	if(allowedIPList.contains(ip)){
-	    return true;
-	}
-	// response.sendError(HttpStatus.FORBIDDEN.value());
-	return false;
+	return true;
     }
 
     @Override
@@ -57,6 +49,28 @@ public class IPInterceptor implements HandlerInterceptor {
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
 	    throws Exception {
+    }
+
+    public void setAuthorizedIps(String authorizedIps) {
+	String[] data = StringUtils.tokenizeToStringArray(authorizedIps, ",", true, true);
+	if (data != null && data.length > 0) {
+	    for (String ip : data) {
+		authorizedIPList.add(ip);
+	    }
+	}
+    }
+
+    public void setDeniedIps(String deniedIps) {
+	String[] data = StringUtils.tokenizeToStringArray(deniedIps, ",", true, true);
+	if (data != null && data.length > 0) {
+	    for (String ip : data) {
+		deniedIPList.add(ip);
+	    }
+	}
+    }
+
+    public void setEnable(boolean enable) {
+	this.enable = enable;
     }
 
 }
