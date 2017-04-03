aea636d051e82abf67303e8d37893ba428b44d7a
chensheng <jrchens@126.com>
2017-04-03 21:39:21 +0800

    add columname to BeanFieldname

--- simple-common/src/main/java/me/simple/util/SqlUtil.java
+++ simple-common/src/main/java/me/simple/util/SqlUtil.java
@@ -20,22 +20,58 @@ public final class SqlUtil {
 	return "%" + StringUtils.trimWhitespace(value) + "%";
     }
 
+    public static String toBeanFieldname(String columname) {
+	if (!StringUtils.hasText(columname)) {
+	    return null;
+	}
+	if (columname.indexOf('_') != -1) {
+	    char[] ca = columname.toCharArray();
+	    boolean bool = false;
+	    StringBuffer buffer = new StringBuffer();
+	    for (char c : ca) {
+		if (Character.isWhitespace(c)) {
+		    continue;
+		}
+		if (c == '_') {
+		    bool = true;
+		    continue;
+		}
+		if (bool) {
+		    buffer.append(Character.toUpperCase(c));
+		} else {
+		    buffer.append(Character.toLowerCase(c));
+		}
+		bool = false;
+	    }
+	    if (buffer.length() > 1)
+		return buffer.substring(0, 1).toLowerCase() + buffer.substring(1);
+	    else
+		return buffer.substring(0, 1).toLowerCase();
+	} else {
+	    if (columname.length() > 1)
+		return columname.substring(0, 1).toLowerCase() + columname.substring(1);
+	    else
+		return columname.substring(0, 1).toLowerCase();
+	}
+    }
+
     public static String getOrder(String value) {
 	return value;
     }
+
     public static String getSort(String value) {
 	return value;
     }
-    
+
     public static String uuid() {
 	String uuid = UUID.randomUUID().toString();
 	StringBuffer buffer = new StringBuffer();
 	buffer.append(uuid.substring(0, 8));
 	buffer.append(uuid.substring(9, 13));
 	buffer.append(uuid.substring(14, 18));
-	buffer.append(uuid.substring(19,23));
+	buffer.append(uuid.substring(19, 23));
 	buffer.append(uuid.substring(24));
-	
+
 	return buffer.toString();
     }
 
